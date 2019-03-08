a765e9e8e977a8923c1cce811e7c9b7e8bd09650
Alexander Bukatko <sashamu77@mail.ru>
2019-03-08 19:30:48 +0300

    First commit

---
+++ Dev1/Dev1/SequenceCheck.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+
+namespace Dev1
+{
+    class SequenceCheck
+    {
+        StringBuilder workingString;
+
+        /// <summary>
+        /// The constructor creates a string from an array of strings.
+        /// </summary>
+        /// <param name="input">Array of strings</param>
+        public SequenceCheck(string[] input)
+        {
+            workingString = new StringBuilder();
+
+            foreach (string word in input)
+            {
+                workingString.Append(word + " ");
+            }
+            workingString.Remove(workingString.Length - 1, 1);
+        }
+
+        /// <summary>
+        /// Compare the next symbol with the previous one and write 
+        /// to the console if they are different.
+        /// </summary>
+        public void CheckSequence()
+        {   
+            for (int i = 0; i < workingString.Length - 1; i++)
+            {
+                StringBuilder sequence = new StringBuilder();
+                sequence.Append(workingString[i]);
+
+                for (int j = i + 1; j < workingString.Length; j++)
+                {
+                    if (workingString[j - 1] != workingString[j])
+                    {
+                        sequence.Append(workingString[j]);
+                        Console.WriteLine(sequence);
+                    }
+                    else break;
+                }
+            }
+        }
+    }
+}
---
+++ Dev1/Dev1/StringOptions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Dev1
+{
+    class StringOptions
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please enter a argument.");
+            }
+            else if(args[0].Length < 2)
+            {
+                Console.WriteLine("Please enter more than two symbols.");
+            }
+            else
+            {
+                SequenceCheck sequencesFinder = new SequenceCheck(args);
+                sequencesFinder.CheckSequence();
+            }
+        }
+    }
+}
