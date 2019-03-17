d1af6020f6b59a2645a74a0a864ba86e8c05d5b6
Alexander Bukatko <sashamu77@mail.ru>
2019-03-17 16:58:53 +0300

    add Dev2

---
+++ Dev2/Dev2/EntryPoint.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Dev2
+{
+    /// <summary>
+    /// Calls all program methods and displays the result.
+    /// </summary>
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length == 0)
+                {
+                    throw new Exception("Error. Please enter argument.");
+                }
+                ConverterToPhonemes converterToPhonemes = new ConverterToPhonemes(args);
+                Console.WriteLine(converterToPhonemes.Transcription());
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An exception: {ex.Message}");
+                Console.WriteLine($"Method: {ex.TargetSite}");
+                Console.WriteLine($"Stack trace: {ex.StackTrace}");
+            }
+        }
+    }
+}
