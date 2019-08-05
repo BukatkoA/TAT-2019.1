d229f141e6048f2181a2b79a346b030e0518ad80
BukatkoA <sashamu77@mail.ru>
2019-08-05 21:45:23 +0300

    Bugs fixed

---
+++ Dev6/Dev6/CommandCountAveragePriceOfBrand.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+
+namespace Dev6
+{
+    /// <summary>
+    /// The command to count average price of a certain type.
+    /// </summary>
+    public class CommandCountAveragePriceOfBrand : ICommand
+    {
+        /// <summary>
+        /// The receiver.
+        /// </summary>
+        private CounterAveragePriceOfBrand counter;
+
+        /// <summary>
+        /// The type of a vehicle.
+        /// </summary>
+        private string brand;
+
+        /// <summary>
+        /// The list of vehicles with their info.
+        /// </summary>
+        private List<VehicleInfoStruct> listOfVehicles;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CommandCountAveragePriceOfBrand"/> class.
+        /// </summary>
+        /// <param name="receiver">
+        /// The receiver.
+        /// </param>
+        /// <param name="listOfVehicles">
+        /// The list of vehicles with their info.
+        /// </param>
+        /// <param name="brand">
+        /// The brand of a vehicle.
+        /// </param>
+        public CommandCountAveragePriceOfBrand(CounterAveragePriceOfBrand receiver, List<VehicleInfoStruct> listOfVehicles, string brand)
+        {
+            this.counter = receiver;
+            this.brand = brand;
+            this.listOfVehicles = listOfVehicles;
+        }
+
+        /// <summary>
+        /// Executes the command.
+        /// </summary>
+        public double Execute() => this.counter.CountAveragePrice(this.listOfVehicles, this.brand);
+    }
+}
