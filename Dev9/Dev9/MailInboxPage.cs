bc3df8a37cef4d32b7ad0a5b386a97510c8e3330
BukatkoA <sashamu77@mail.ru>
2019-08-08 23:40:54 +0300

    Bugs Fixed

---
+++ Dev9/Dev9/MailInboxPage.cs
@@ -0,0 +1,67 @@
+﻿using OpenQA.Selenium;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Dev9
+{
+    /// <summary>
+    /// Inbox page.
+    /// </summary>
+    public class MailInboxPage
+    {
+        /// <summary>
+        /// The WebDriver.
+        /// </summary>
+        private IWebDriver driver;
+
+        /// <summary>
+        /// The button for writing a new letter.
+        /// </summary>
+        public IWebElement WriteNewLetterButton => this.driver.FindElement(By.XPath("//a[@data-name='compose']"));
+
+        /// <summary>
+        /// The latest mail in the inbox.
+        /// </summary>
+        public List<IWebElement> Mails => this.driver.FindElements(By.XPath("//div[@class='b-datalist__body']/div")).ToList();
+
+        /// <summary>
+        /// Initializes a new instance of the MailInboxPage class.
+        /// </summary>
+        /// <param name="driver">
+        /// The WebDriver.
+        /// </param>
+        public MailInboxPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        /// <summary>
+        /// Proceed to the writing of a new letter.
+        /// </summary>
+        /// <returns>
+        /// The WriteNewLetterPage.
+        /// </returns>
+        public WriteNewLetterPage WriteNewLetter()
+        {
+            this.WriteNewLetterButton.Click();
+
+            return new WriteNewLetterPage(this.driver);
+        }
+
+        /// <summary>
+        /// Goes to the reading page for the mail.
+        /// </summary>
+        /// <param name="mail">
+        /// The mail to read.
+        /// </param>
+        /// <returns>
+        /// The MailReadPage.
+        /// </returns>
+        public MailReadPage ReadMail(IWebElement mail)
+        {
+            mail.Click();
+
+            return new MailReadPage(this.driver);
+        }
+    }
+}
---
+++ Dev9/Dev9/PersonalDataPage.cs
@@ -0,0 +1,56 @@
+﻿using OpenQA.Selenium;
+
+namespace Dev9
+{
+    /// <summary>
+    /// The personal data page.
+    /// </summary>
+    public class PersonalDataPage
+    {
+        /// <summary>
+        /// The WebDriver.
+        /// </summary>
+        private IWebDriver driver;
+
+        /// <summary>
+        /// The nickname box.
+        /// </summary>
+        public IWebElement NicknameBox => this.driver.FindElement(By.XPath("//input[@id='NickName']"));
+
+        /// <summary>
+        /// The submit button.
+        /// </summary>
+        public IWebElement SubmitButton => this.driver.FindElement(By.XPath("//span[text() = 'Сохранить']"));
+
+        /// <summary>
+        /// Initializes a new instance of the PersonalDataPage class.
+        /// </summary>
+        /// <param name="driver">
+        /// The WebDriver.
+        /// </param>
+        public PersonalDataPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        /// <summary>
+        /// The change nickname.
+        /// </summary>
+        /// <param name="newNickname">
+        /// The new nickname.
+        /// </param>
+        /// <returns>
+        /// The PersonalDataPage.
+        /// </returns>
+        public PersonalDataPage ChangeNickname(string newNickname)
+        {
+            this.NicknameBox.Clear();
+            this.NicknameBox.SendKeys(newNickname);
+            this.SubmitButton.Click();
+            // Return to the userdata page
+            this.driver.FindElement(By.XPath("//a[@data-name='userinfo']")).Click();
+
+            return this;
+        }
+    }
+}
